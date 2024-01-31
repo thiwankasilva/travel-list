03449e374810dbc187fe0d58bf0b33a65a1b2e4c
thiwankasilva <64344159+thiwankasilva@users.noreply.github.com>
2024-02-01 00:14:25 +0530

    Added Clear List Button

--- src/App.js
+++ src/App.js
@@ -20,6 +20,16 @@ export default function App() {
     );
   }
 
+  function handleClearList()
+  {
+    const confirmed = window.confirm("Are you sure");
+
+    if(confirmed)
+    {
+      setItems([]);
+    }
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -28,6 +38,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggle={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -76,14 +87,39 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackagingList({ items, onDeleteItem, onToggle }) {
+function PackagingList({ items, onDeleteItem, onToggle,onClearList }) {
+  const [sortBy,setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if(sortBy === "description")
+  {
+    sortedItems = items.slice().sort((a,b) => a.description.localCompare(b.description) )
+  }
+
+  if(sortBy === "packed")
+  {
+    sortedItems = items
+    .slice()
+    .sort((a,b) => Number(a.packed) - Number(b.packed))
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
-          <Item item={item} onDeleteItem={onDeleteItem} onToggle={onToggle} />
+        {sortedItems.map((item) => (
+          <Item item={item} onDeleteItem={onDeleteItem} onToggle={onToggle} key={item.id} />
         ))}
       </ul>
+      <div className="actions">
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value= "input"> Sort by input order</option>
+        <option value= "description"> Sort by description </option>
+        <option value= "packed"> Sort by packed </option>
+      </select>
+      <button onClick={() => onClearList()}>Clear List</button>
+
+      </div>
     </div>
   );
 }
